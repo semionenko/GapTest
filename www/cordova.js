78a03834ec6832358816532d31662ee0f439db8f
semionenko <semionenko@inexika.com>
2016-03-31 13:55:51 +0600

    minor changes

--- www/cordova.js
+++ www/cordova.js
@@ -1,6 +1,6 @@
 // Platform: ios
 // ded62dda172755defaf75378ed007dc05730ec22
-/*
+/* 
  Licensed to the Apache Software Foundation (ASF) under one
  or more contributor license agreements.  See the NOTICE file
  distributed with this work for additional information
